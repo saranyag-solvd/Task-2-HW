188da7921235c915c5d8ac0b3141cceea2de00a3
Saranya Gopalakrishnan <sgopalakrishnan.laba@solvd.com>
2023-05-01 13:54:08 -0400

    HW5 -formatted

--- src/com/solvd/travelagency/exceptions/TravelAgencyException.java
+++ src/com/solvd/travelagency/exceptions/TravelAgencyException.java
@@ -1,7 +1,7 @@
 package com.solvd.travelagency.exceptions;
 
-public class TravelAgencyException extends Exception{
-    public TravelAgencyException(String errorMessage){
+public class TravelAgencyException extends Exception {
+    public TravelAgencyException(String errorMessage) {
         super(errorMessage);
     }
 }
